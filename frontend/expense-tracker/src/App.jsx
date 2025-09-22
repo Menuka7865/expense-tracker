f991a0c1f36a7e175e029c36fd413815513acec1
Menuka7865 <menukasandaruwan456@gmail.com>
2025-09-22 21:55:06 +0530

    project folder structure created

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+    <div className='text-center mt-5 font-bold text-2xl text-blue-600'>
+      <h1>Hello, World!</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;

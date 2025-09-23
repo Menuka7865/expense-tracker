98f6e1b5112a32df215d0baaec29fa1fa3c84281
Menuka7865 <menukasandaruwan456@gmail.com>
2025-09-23 14:10:22 +0530

    backend for upload image from local storage

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -26,4 +26,12 @@ router.post("/login", loginUser);
 // Get user profile
 router.get("/getUser",protect, getUserInfo);
 
+router.post("/upload", upload.single('profileImage'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: 'No file uploaded' });
+    }
+    const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+    res.status(200).json({ imageUrl });
+});
+
 module.exports = router;

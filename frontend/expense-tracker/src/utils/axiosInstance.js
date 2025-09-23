9b8d022f4c0e564b28d41f89c46dfb113e95a255
Menuka7865 <menukasandaruwan456@gmail.com>
2025-09-24 00:16:52 +0530

    Add dashboard API and frontend utilities

---
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) =>{return Promise.reject(error)} 
+);
+
+axiosInstance.interceptors.response.use(
+  (response) =>{return response;}, 
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Handle unauthorized access, e.g., redirect to login
+      console.error("Unauthorized access - perhaps redirect to login?");
+         window.location.href = "/login"; // Uncomment to enable redirection
+    }
+    return Promise.reject(error);
+  }
+);
+export default axiosInstance;
